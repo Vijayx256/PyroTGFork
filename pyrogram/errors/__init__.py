bc04b6eca16253c4bab8984fffe28827f8398eb4
Vijayx256 <neha256rajput@gmail.com>
2026-06-19 16:15:23 +0530

    Update __init__.py

--- pyrogram/errors/__init__.py
+++ pyrogram/errors/__init__.py
@@ -1,23 +1,31 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Pyrofork - Telegram MTProto API Client Library for Python
 #  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrofork.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrofork is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrofork is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from .exceptions import *
-from .rpc_error import UnknownError
+EXCEPTION_AVAIL = False
+try:
+    from .exceptions import *
+except ImportError:
+    pass
+else:
+    EXCEPTION_AVAIL = True
+from .pyromod import *
+from .rpc_error import RPCError, UnknownError
 
 
 class BadMsgNotification(Exception):
@@ -63,3 +71,15 @@ class CDNFileHashMismatch(SecurityError):
 
     def __init__(self, msg: str = None):
         super().__init__("A CDN file hash mismatch has occurred." if msg is None else msg)
+
+__all__ = [
+    "BadMsgNotification",
+    "SecurityError",
+    "SecurityCheckMismatch",
+    "CDNFileHashMismatch",
+    "RPCError",
+    "UnknownError"
+]
+if EXCEPTION_AVAIL:
+    __all__.extend(exceptions.__all__)
+__all__.extend(pyromod.__all__)
