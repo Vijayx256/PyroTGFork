66daffb9a6d7200353d575a375498997a95b7bd2
Vijayx256 <neha256rajput@gmail.com>
2026-06-19 16:55:49 +0530

    Update __init__.py

--- pyrogram/errors/pyromod/__init__.py
+++ pyrogram/errors/pyromod/__init__.py
@@ -1,4 +1,4 @@
 from .listener_stopped import ListenerStopped
 from .listener_timeout import ListenerTimeout
 
-__all__ = ["ListenerStopped", "ListenerTimeout"]
+all = ["ListenerStopped", "ListenerTimeout"]
